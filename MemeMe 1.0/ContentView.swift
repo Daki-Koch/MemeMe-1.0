3a3c867a01c544221d7e64e76b29f56874e1ded2
Daki-Koch <57409868+Daki-Koch@users.noreply.github.com>
2022-09-05 11:42:00 +0200

    Initial Commit

---
+++ MemeMe 1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemeMe 1.0
+//
+//  Created by David Koch on 05.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemeMe 1.0/MemeMe_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMe_1_0App.swift
+//  MemeMe 1.0
+//
+//  Created by David Koch on 05.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MemeMe_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
